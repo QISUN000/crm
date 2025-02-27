ba0ba89a3abc2ca306eccdd6dd48eb5e7f399a66
QI SUN <lukesun2023@gmail.com>
2025-02-26 21:24:51 -0600

    Initial commit: Simple CRM with Customer model, Active Admin, and customer views

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
   
   validates :full_name, presence: true
   validates :email_address, format: { with: URI::MailTo::EMAIL_REGEXP, allow_blank: true }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email_address", "full_name", "id", "notes", "phone_number", "updated_at"]
+  end
+  
+  def self.ransackable_associations(auth_object = nil)
+    ["image_attachment", "image_blob"]
+  end
+  
 end
